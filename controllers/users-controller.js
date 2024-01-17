89ba59362b3808789c835de48173df2266a3794a
21120321 <gamefun206@gmail.com>
2024-01-17 16:17:22 +0700

    jwt

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,6 +1,10 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const Student = require("../models/Student.m");
 const Tutor = require("../models/Tutor.m");
+
+require('dotenv').config();
 
 const HttpError = require('../models/http-error');
 const {validationResult} = require('express-validator');
@@ -27,27 +31,68 @@ const createUser = async (req,res, next) =>{
         password: await bcrypt.hash(password, 4),
         name
     };
-    const signUpResult = await User.create(newUser);
-    req.session.userId = signUpResult._id.toString();
+    let signUpResult
+    try {
+        signUpResult = await User.create(newUser);
+    }
+    catch (err) {
+        return next (new HttpError ('Can not sign up', 500));
+    }
+    
+    try {
+        jwt.sign(
+            {
+                userId : newUser.id,
+                email : newUser.email,
+                role 
+            }
+        )
+    }catch (err) {
+        return next (new HttpError ('Can not sign up', 500));
+    }
+    
     res.status(201).json(signUpResult)
 }
 
 const signInHandler = async (req,res,next)=>{
     const {email, password} = req.body;
-    const User = extractRole(email) === "student" ? Student : Tutor;
+    const role = extractRole(email);
+    const User = role === "student" ? Student : Tutor;
     const identifierUser = await User.findOne({email: email});
 
     if(!identifierUser) {
-        return next (new HttpError('Wrong email'))
+        return next (new HttpError('Wrong email',420))
     }
 
     const match = await bcrypt.compare(password, identifierUser.password);
     if(!match){
-        return next( new HttpError('Wrong password'))
+        return next( new HttpError('Wrong password', 420))
+    }
+
+    try {
+        jwt.sign(
+            {
+                userId : identifierUser.id,
+                email : identifierUser.email,
+                role 
+            },
+            process.env.JWT_KEY,
+            {expiresIn : "1h"}
+        )
+    }
+    catch (err ) {
+        return next( new HttpError('Can not login', 510))
     }
 
     req.session.userId = identifierUser._id.toString();
-    res.status(200).json({message : "Login sucessfully"});
+    res.status(200).json(
+        {
+            message : "Login sucessfully" ,  
+            user : {
+                ...identifierUser ,
+                role : extractRole(email)
+            }
+    });
 }
 exports.createUser = createUser;
 exports.signInHandler = signInHandler;
