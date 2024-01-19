e402943f88a001fc26e2a9077bf17e6c2834f03e
Quoc Hoang <hoangvanquoc923@gmail.com>
2024-01-19 18:03:10 +0700

    Student's my courses page done

--- src/components/Student/CourseCard.jsx
+++ src/components/Student/CourseCard.jsx
@@ -4,14 +4,14 @@ export default function CourseCard({course}) {
     const defaultCourseImage = "default-course.jpg";
     return (
         <>
-            <div class="col-lg-12 col-md-12 wow zoomIn" data-wow-delay="0.1s">
-                <NavLink class="position-relative d-block overflow-hidden" to="">
+            <div className="col-lg-12 col-md-12 wow zoomIn" data-wow-delay="0.1s">
+                <NavLink className="position-relative d-block overflow-hidden" to="">
                     <div style={{height: "200px", opacity: "0.7"}}>
-                        <img class="img-fluid" src={course.image ? course.image: defaultCourseImage} alt="" width="100%" style={{objectPosition: "0px -125px"}} />
+                        <img className="img-fluid" src={course.image ? course.image: defaultCourseImage} alt="" width="100%" style={{objectPosition: "0px -125px"}} />
                     </div>
-                    <div class=" text-md-start position-absolute top-0 start-0 end-0" style={{margin: "1px", height: "200px"}}>
-                        <h5 class="m-0 px-3 py-2">{course.name}</h5>
-                        <h6 class="text-primary px-3">Time: {course.dateStart} - {course.dateEnd}</h6>
+                    <div className=" text-md-start position-absolute top-0 start-0 end-0" style={{margin: "1px", height: "200px"}}>
+                        <h5 className="m-0 px-3 py-2">{course.name}</h5>
+                        <h6 className="text-primary px-3">Time: {course.dateStart} - {course.dateEnd}</h6>
                     </div>
                 </NavLink>
             </div>
--- src/pages/student/Courses.jsx
+++ src/pages/student/Courses.jsx
@@ -1,45 +1,53 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../shared/context/auth-context";
 import { responsiveFontSizes } from "@mui/material";
-import  CourseCard  from "../../components/Student/CourseCard";
+import CourseCard from "../../components/Student/CourseCard";
 import CourseQuickLink from "../../components/Student/CourseQuickLink";
-
+import { useHttpClient } from "../../hooks/http-hook";
+import LoadingSpinner from "../../components/shared/UIElements/LoadingSpinner";
+import ErrorModal from "../../components/shared/UIElements/ErrorModal";
 export default function Courses() {
     const { userId: studentId } = useContext(AuthContext);
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
     const [courseList, setCourseList] = useState([]);
-
+    //console.log("student id in course", studentId);
     useEffect(() => {
-        async function fetchCourses() {
-            const courses = fetch("http://localhost:5000/api/student/courses", {
-                method: 'POST',
-                body: JSON.stringify({ studentId })
-            })
-                .then(response => {
-                    const list = response.json().courses;
-                    setCourseList(list);
-                    console.log(list)
-                })
+        async function fetchCode() {
+            try {
+                const data = await sendRequest(`http://localhost:5000/api/student/courses`, 'POST', {
+                    'Content-Type': 'application/json'
+                }, JSON.stringify({ studentId }));
+                setCourseList(data.courses);
+            }
+            catch (error) {
+            }
         }
-        fetchCourses();
-    }, []);
+        fetchCode();
+    }, [sendRequest]);
 
     return (
-        <div className="container-xxl py-5 category">
-            <div className="container">
-                <div className="row g-3">
-                    <div className="col-lg-8 col-md-6 bg-white" >
-                        <div className="row g-3">
-                            <div className="text-md-start wow fadeInUp " data-wow-delay="0.1s" >
-                                <h1 className="">My Courses</h1>
+        <>
+            {isLoading && (<LoadingSpinner asOverlay />)}
+            {<ErrorModal error={error} onClear={clearError} />}
+            <div className="container-xxl py-5 category">
+                <div className="container">
+                    <div className="row g-3">
+                        <div className="col-lg-8 col-md-6 bg-white" >
+                            <div className="row g-3">
+                                <div className="text-md-start wow fadeInUp " data-wow-delay="0.1s" >
+                                    <h1 className="">My Courses</h1>
+                                </div>
+                                {courseList?.length > 0 && courseList?.map(course => (
+                                    <CourseCard key={course._id} course={course} />
+                                ))}
                             </div>
-                            {courseList.map(course => (
-                                <CourseCard course={course} />
-                            ))}
                         </div>
-                        <CourseQuickLink courses={courseList} />
+                        <div className="col-lg-4 col-md-6 bg-white" >
+                            <CourseQuickLink courses={courseList} />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
